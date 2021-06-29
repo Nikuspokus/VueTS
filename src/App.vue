5810c77c2afceeeaacb538174ca9ef5692ff11e2
Marquillier <nicolas.marquillier@gmail.com>
2021-06-29 17:21:23 +0200

    Vue3 - TS -- types & props - FINI

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,62 @@
 <template>
   <div class="app">
-    <p>{{ name }} - {{ age }}</p>
-    <button @click="changeName('zelda')">change name</button>
-    <button @click="changeAge(30)">change age</button>
+    <JobList :jobs="jobs"/>
   </div>
 </template>
 
 <script lang="ts">
-
-import { defineComponent, reactive, toRefs } from "vue";
+import { defineComponent, ref,  } from "vue";
+import JobList from "./components/JobsList.vue";
+import Job from "./types/Job";
 
 export default defineComponent({
   name: "App",
-  components: {},
+  components: { JobList },
   setup() {
-    const state = reactive({
-      name: 'Link',
-      age: '35' as string | number
-    })
-    return { ...toRefs(state) }
+    // const state = reactive({
+    //   name: 'Link',
+    //   age: '35' as string | number
+    // })
+    // state.name = 999 //cannot change type
+    // state.name = 'Shaun'
+
+    // state.age = '26'
+    // return { ...toRefs(state) }
+
+    // const name = ref('Link')
+    // const age = ref<number | string>(35)
+
+    // return {name, age}
+
+    const jobs = ref<Job[]>([
+      {
+        title: "farm worker",
+        location: "lon lon ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "quarryman",
+        location: "death mountain",
+        salary: 40000,
+        id: "2",
+      },
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      { title: "fisherman", location: "lake hylia", salary: 21000, id: "4" },
+      {
+        title: "prison guard",
+        location: "gerudo valley",
+        salary: 32000,
+        id: "5",
+      },
+    ]);
+    return { jobs };
   },
-  methods: {
-    changeName(name: string) {
-      this.name = name
-      return name
-    },
-    changeAge(age: string | number) {
-      this.age = age
-      return age 
-    }
-  }
 });
 </script>
 
---
+++ src/components/JobsList.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="job-list">
+    <ul>
+      <li v-for="job in jobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <p>{{ job.salary }} €uros</p>
+        </div>
+        <div class="description">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam
+            explicabo sed recusandae accusantium ducimus ipsa similique
+            voluptatum, quaerat, unde earum laborum architecto eius dolorem iure
+            assumenda tempora quibusdam nesciunt. Dolorum?
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Job from "@/types/Job";
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  props: {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>,
+    },
+  },
+});
+</script>
+
+<style scoped>
+.job-list {
+  max-width: 960px;
+  margin: 40px auto;
+}
+.job-list ul {
+  padding: 0;
+}
+.job-list li {
+  list-style-type: none;
+  background: white;
+  padding: 16px;
+  margin: 16px 0;
+  border-radius: 4px;
+}
+.job-list h2 {
+  margin: 0 0 10px;
+  text-transform: capitalize;
+}
+.salary {
+  display: flex;
+}
+.salary img {
+  width: 30px;
+}
+.salary p {
+  color: #17bf66;
+  font-weight: bold;
+  margin: 10px 4px;
+}
+</style>
