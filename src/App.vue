5d6dced78e3b89f072308d3148527e6e19f6f36d
Marquillier <nicolas.marquillier@gmail.com>
2021-06-29 15:42:17 +0200

    Vue3 - TS -- composition API & TypeScript

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <template>
-<div class="app">
-  Hello, Hyrule
-</div>
+  <div class="app">
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('zelda')">change name</button>
+    <button @click="changeAge(30)">change age</button>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+
+import { defineComponent, reactive, toRefs } from "vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
+  name: "App",
+  components: {},
+  setup() {
+    const state = reactive({
+      name: 'Link',
+      age: '35' as string | number
+    })
+    return { ...toRefs(state) }
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name
+      return name
+    },
+    changeAge(age: string | number) {
+      this.age = age
+      return age 
+    }
   }
 });
 </script>
 
-<style>
-
-</style>
+<style></style>
